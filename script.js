ef3a04bcd6e3a56933bd8658682624b5e99d0fe2
Mike <michael.fomin.89@gmail.com>
2020-11-26 23:21:08 +0300

    Promis hot-fix userName

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 let userName = window.location.search;
-let user = 'Gard0';
 let url = `https://api.github.com/users/`;
+let user = 'Gard0';
 
 function searchName() {
   if (userName) {
@@ -10,47 +9,46 @@ function searchName() {
       (userName.slice(1));
   }
 }
-searchName();
+searchName(userName);
 
-let preloader = document.getElementById('preloader');
+
 setTimeout(function () {
+  let preloader = document.querySelector('.container');
   preloader.classList.add('invisible');
-}, 4000);
 
-const setDate = new Date();
-let date = new Promise((resolve, reject) => {
-  setTimeout(() => date ? resolve(setDate) : reject('Время не опредено'), 1000);
-});
-const getUrl = new Promise((resolve, reject) => {
-  setTimeout(() => getUrl ? resolve(url) : reject('URL не найден'), 2000);
-});
-const getName = new Promise((resolve, reject) => {
-  setTimeout(() => getName ? resolve(user) : reject('Пользователь не найден2'), 2000);
-});
+  let date = new Date();
+  let setDate = new Promise((resolve, reject) => {
+    setTimeout(() => setDate ? resolve(date) : reject('Время не опредено'), 3000);
+  });
+  let getUrl = new Promise((resolve, reject) => {
+    setTimeout(() => getUrl ? resolve(url) : reject('URL не найден'), 2000);
+  });
+  let getName = new Promise((resolve, reject) => {
+    setTimeout(() => getName ? resolve(user) : reject('Пользователь не найден2'), 1000);
+  });
 
-Promise.all([getUrl, getName])
-  .then(([url, userName]) => fetch(`${url}${userName}`)) //
-  .then(res => res.json())
-  .then(json => {
-    console.log(json);
-    const div = document.createElement('div');
-    document.body.append(div);
-    const userName = document.createElement('a');
-    if (json.name !== undefined) {
-      userName.textContent = json.name;
-      userName.href = json.html_url;
-    } else {
-      userName.textContent = 'Information not available probably .json'
+  Promise.all([getUrl, getName, setDate])
+    .then(([url, user]) => fetch(`${url}${user}`))
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      const div = document.createElement('div');
+      document.body.append(div);
+      const userName = document.createElement('a');
+      if (json.name !== undefined) {
+        userName.textContent = json.name;
+        userName.href = json.html_url;
+      } else {
+        userName.textContent = "Information undefined."
+      }
+      div.append(userName);
+      const userBio = document.createElement('h3');
+      userBio.textContent = json.bio;
+      div.append(userBio);
+      const userImg = document.createElement('img');
+      userImg.src = json.avatar_url;
+      div.append(userImg);
+      document.body.append(date);
     }
-    div.append(userName);
-    const userBio = document.createElement('h3');
-    userBio.textContent = json.bio;
-    div.append(userBio);
-    const userImg = document.createElement('img');
-    userImg.src = json.avatar_url;
-    userImg.alt = 'user_avatar_image';
-    div.append(userImg);
-    document.body.append(setDate);
-  }
-  )
-  .catch(err => console.log('Information not available'));
+    )
+}, 2000);
